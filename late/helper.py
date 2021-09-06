d329c9de0bb3452428d894655e33c6805f1fbb26
aheirman <48590738+aheirman@users.noreply.github.com>
2021-09-06 10:20:53 +0200

    Add helper

---
+++ late/helper.py
@@ -0,0 +1,9 @@
+from inspect import getframeinfo, stack
+
+import builtins
+import os
+import sys
+
+def print(message, sep=' ', end='\n', file=sys.stdout, flush=False):
+    caller = getframeinfo(stack()[1][0])
+    builtins.print(os.path.basename(caller.filename) + "(" + (caller.function + ": ").ljust(40, " ") + f'{caller.lineno:03d}' + "): \t" + str(message), sep=sep, end=end, file=file, flush=flush)
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from late.tests import *
+from late.helper import print
 
 unittest.main()
+
+def __readFile(url: str):
+    with open(url) as f:
+         return f.readlines()
+
+def __parseFile(lines: list[str]):
+    pass
+
+def handle(ufl: str):
+    productions = __parseFile(__readFile(url))
